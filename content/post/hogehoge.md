adab3b6535a28c7bb329a1917dd6791481511961
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-10-31 12:59:20 +0900

    recompse my site

--- content/post/hogehoge.md
+++
@@ -1,7 +0,0 @@
-+++
-date = "2015-10-30T18:06:51+09:00"
-draft = true
-title = "hogehoge"
-
-+++
-
--- gh-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git commit --allow-empty -m "noun"
-git push -u origin master
-git checkout -b gh-pages
-git commit --allow-empty -m "noun"
-git push -u origin gh-pages
